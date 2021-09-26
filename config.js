6f14ded0b7a689c4e5e0b913ccb7436ae568d941
yeuuyenratnhieu <91424779+yeuuyenratnhieu@users.noreply.github.com>
2021-09-26 22:28:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Uyên à!',
+    introDesc: `Vậy là chúng ta đã đi qua được 1000days rồi đấy
+    Vậy là em đã thuộc về anh được 1000 ngày ròi
+    Hôm nay đổi mới xíu nên làm thế này
+    Mong là em thích`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Em có muốn tiếp tục nắm tay anh đi tiếp không 😙',
+    desc: 'Em có muốn được làm mẹ của con anh không ',
+    btnYes: 'Tất nhiên rồi anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Em sẽ tha lỗi cho những gì đã xảy ra và tiếp tục đi cùng anh chứ',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Dạ em đồng ý mãi yêu anh  <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Chúc em có một ngày kỉ niệm vui vẻ nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
